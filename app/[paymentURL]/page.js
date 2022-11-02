71fc8e8911a4bd842618706449aaabe8309c0401
sammy freddie <sammyfreddie@Mac-Studio.attlocal.net>
2022-11-02 16:43:35 -0700

    initial

---
+++ app/[paymentURL]/page.js
@@ -0,0 +1,35 @@
+import MenuLinks from "../(components)/MenuLinks";
+import React from "react";
+
+export async function generateStaticParams(){
+    const unpaidPayments = await fetch("https://server-neon-tau.vercel.app/api/unpaid")
+    return unpaidPayments.map((paymentObj) => ({paymentURL : paymentObj.url}))
+}
+
+async function getUnpaid(){
+    const unpaid = await fetch("https://server-neon-tau.vercel.app/api/unpaid")
+    return unpaid.json()
+}
+
+export default async function PaymentURL({params}){
+
+    const unpaid = await getUnpaid()
+
+
+    let menuLinksObjArray = [{name: "<------", url: "/"}, {name: " ", url: "/"}]
+    for(let i = 0; i < unpaid.length;i++) {
+        if (unpaid[i].url != params.paymentURL) {
+            menuLinksObjArray.push({name: " ", url: "/"})
+        } else if (unpaid[i].url === params.paymentURL) {
+            menuLinksObjArray.push({name: unpaid[i].name + ": $" + unpaid[i].amount, url: "/" });
+            break;
+        }
+    }
+
+    return(
+            <div>
+                <MenuLinks objArray={menuLinksObjArray}/>
+
+            </div>
+    )
+}
